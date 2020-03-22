452a1b86af5ef9cdc66565436542ba499f0bb439
Alejandro Castello <a.castello.pas@gmail.com>
2020-03-22 12:09:11 -0500

    Strip unused functions. Hardcode placeholder assignment

--- main.c
+++ main.c
@@ -9,134 +9,90 @@
 
 #define WINE_SYSTEM_PREFIX "C:\\windows\\system32\\"
 
-Display *(*real_XOpenDisplay)(const char *display_name);
-
 Window (*real_XCreateWindow)(Display *display, Window parent, int x, int y,
         unsigned int width, unsigned int height, unsigned  int  border_width,
         int depth, unsigned int class,  Visual *visual, unsigned long valuemask,
         XSetWindowAttributes *attributes);
 
-int (*real_XMapWindow)(Display *display, Window window);
-
-Display *XOpenDisplay(const char* display_name)
-{
-    FILE *log = fopen("/home/alex/log", "a+");
-    fprintf(log, "DISPLAY INIT %s\n", program_invocation_name);
-    fclose(log);
-    return real_XOpenDisplay(display_name);
-}
+void (*real_wine_init)(int argc, char *argv[], char *error, int error_size);
 
 Window placeholder;
+
 void create_placeholder(void)
 {
-    if (!placeholder) {
-        printf("CREprogname: %s\n", program_invocation_name);
-        XInitThreads();
-        Display *dpy = real_XOpenDisplay(NULL);
-
-        XSetWindowAttributes orig_attributes;
-        placeholder = real_XCreateWindow(dpy, DefaultRootWindow(dpy), 10,
-                10, 100, 100, 0, CopyFromParent, CopyFromParent, CopyFromParent,
-                0, &orig_attributes);
-        real_XMapWindow(dpy, placeholder);
-        XFlush(dpy);
+    if (placeholder) {
+        fputs("!! Trying to recreate placeholder window", stderr);
+        exit(1);
     }
+    XInitThreads();
+    Display *dpy = XOpenDisplay(NULL);
+    placeholder = real_XCreateWindow(dpy, DefaultRootWindow(dpy), 10, 10,
+            100, 100, 0, CopyFromParent, CopyFromParent, CopyFromParent, 0,NULL);
+    XMapWindow(dpy, placeholder);
+    XFlush(dpy);
 }
 
-int create_window_count = 0;
 Window XCreateWindow(Display *display, Window parent, int x, int y,
         unsigned int width, unsigned int height, unsigned  int  border_width,
         int depth, unsigned int class,  Visual *visual, unsigned long valuemask,
         XSetWindowAttributes *attributes)
 {
-    printf("progname: %s\n", program_invocation_name);
-    if (
-            create_window_count < 8 &&
-            parent == XDefaultRootWindow(display)
-       ) {
-        create_window_count++;
-        if (placeholder && create_window_count == 8) {
-            XChangeWindowAttributes(display, placeholder,
-                    valuemask, attributes);
-            printf("win: 0x%lx\n", placeholder);
-            return placeholder;
-        }
+    static int top_ignored = 8;
+    int top_window = parent == XDefaultRootWindow(display);
+    if (top_window) {
+        top_ignored--;
     }
 
-    Window win = real_XCreateWindow(display, parent, x, y, width, height,
+    if (placeholder && top_window && top_ignored == 0) {
+        XChangeWindowAttributes(display, placeholder, valuemask, attributes);
+        return placeholder;
+    }
+
+    return real_XCreateWindow(display, parent, x, y, width, height,
             border_width, depth, class, visual, valuemask, attributes);
-    printf("win! 0x%lx\n", win);
-    return win;
 }
 
-int XMapWindow(Display *display, Window window)
-{
-    int result = real_XMapWindow(display, window);
-    XWindowAttributes attrs;
-    XGetWindowAttributes(display, window, &attrs);
-
-    Window root;
-    Window parent;
-    Window *children[1024];
-    unsigned int n;
-    XQueryTree(display, window, &root, &parent, (Window **) &children, &n);
-    printf("map 0x%lx (0x%lx): %d %d\n", window, parent, attrs.override_redirect, attrs.map_state);
-
-    return result;
-}
-
-void (*real_wine_init)(int argc, char *argv[], char *error, int error_size);
-
-int wine_init_called = 0;
 void wine_init(int argc, char *argv[], char *error, int error_size)
 {
-    FILE *log = fopen("/home/alex/log", "a+");
-    fprintf(log, "WINE_INIT %s\n", program_invocation_name);
-    int i;
-    for (i = 0; i < argc; i++) {
-        fprintf(
-                log,
-                "    %d %s %d\n",
-                i,
-                argv[i],
-                strncmp(argv[i], WINE_SYSTEM_PREFIX, sizeof(WINE_SYSTEM_PREFIX)-1)
-               );
-    }
     if (
             argc > 0 &&
-            0 != strncmp(
+            strncmp(
                 argv[1],
                 WINE_SYSTEM_PREFIX,
-                sizeof(WINE_SYSTEM_PREFIX) - 1)
+                sizeof(WINE_SYSTEM_PREFIX) - 1
+                )
        ) {
-        wine_init_called = 1;
-        create_placeholder();
+        if (!placeholder)
+            create_placeholder();
     }
-    fclose(log);
     real_wine_init(argc, argv, error, error_size);
 }
 
+#define DLCHECKERROR(var) \
+    do {                               \
+        if (!var) {                    \
+            fputs(dlerror(), stderr);  \
+            fputs(dlerror(), stderr);  \
+            exit(1);                   \
+        }                              \
+    } while (0);
+
 void __attribute__ ((constructor)) __init(void)
 {
-    FILE *log = fopen("/home/alex/log", "a+");
-    fprintf(log, "INIT %d %s\n", getpid(), program_invocation_name);
-    fclose(log);
-    printf("TOPprogname: %s\n", program_invocation_name);
-    void *realX11 = dlopen("libX11.so", RTLD_NOW | RTLD_GLOBAL);
-    if (!realX11) {
-        fputs(dlerror(), stderr);
-        fputs(dlerror(), stderr);
-        exit(1);
-    }
-    real_XOpenDisplay = dlsym(realX11, "XOpenDisplay");
-    real_XCreateWindow = dlsym(realX11, "XCreateWindow");
-    real_XMapWindow = dlsym(realX11, "XMapWindow");
+    void *real_X11 = dlopen("libX11.so", RTLD_NOW | RTLD_GLOBAL);
+    DLCHECKERROR(real_X11);
+    real_XCreateWindow = dlsym(real_X11, "XCreateWindow");
+    DLCHECKERROR(real_XCreateWindow);
 
-    void *realwine = dlopen("libwine.so", RTLD_NOW | RTLD_GLOBAL);
-    if (!realX11) {
-        fputs(dlerror(), stderr);
-        fputs(dlerror(), stderr);
-        exit(1);
+    char *execname = basename(program_invocation_name);
+    if (
+            !strcmp(execname, "wine") ||
+            !strcmp(execname, "wine32") ||
+            !strcmp(execname, "wine64")
+       ){
+        void *real_wine = dlopen("libwine.so", RTLD_NOW | RTLD_GLOBAL);
+        DLCHECKERROR(real_wine);
+        real_wine_init = dlsym(real_wine, "wine_init");
+        DLCHECKERROR(real_wine_init);
     }
-    real_wine_init = dlsym(realwine, "wine_init");
 }
