e74914112ed0819bc93b44a8f5e4df00f5ae9705
Alejandro Castello <a.castello.pas@gmail.com>
2020-03-24 13:45:42 -0500

    Implement saving the index after visibly mapping a window

--- config.c
+++ config.c
@@ -1,13 +1,20 @@
 #define _GNU_SOURCE
 #include <glib.h>
 #include <stdio.h>
+#include <sys/file.h>
+#include <unistd.h>
 
 #include "xahead.h"
 
 #define DEFAULT_GROUP "default"
 #define WINE_GROUP "wine"
 
-const gchar *key_file_name = "/home/alex/.config/xahead";
+gchar *key_file_name = "/home/alex/.config/xahead";
+
+void configure_key_file_name(char *filename)
+{
+    key_file_name = filename;
+}
 
 int load_index(char *progname, int is_wine)
 {
@@ -42,3 +49,57 @@ int load_index(char *progname, int is_wine)
             &error
             );
 }
+
+void save_index(char *progname, int is_wine, int index)
+{
+    g_autoptr(GError) error = NULL;
+    g_autoptr(GKeyFile) key_file = g_key_file_new();
+
+    int fd;
+    if (!(fd = open(key_file_name, 0))) {
+        perror("save_index");
+        return;
+    }
+
+    if (!flock(fd, LOCK_SH)) {
+        perror("save_index");
+        return;
+    }
+
+    if (
+            !g_key_file_load_from_file(
+                key_file,
+                key_file_name,
+                G_KEY_FILE_KEEP_COMMENTS | G_KEY_FILE_KEEP_TRANSLATIONS,
+                &error
+                )
+       ) {
+        fprintf(stderr, "Cannot read key file \"%s\": %s\n", key_file_name, error->message);
+        return;
+    }
+    g_key_file_set_integer(
+            key_file,
+            is_wine ? WINE_GROUP : DEFAULT_GROUP,
+            progname,
+            index
+            );
+    if (
+            !g_key_file_save_to_file(
+                key_file,
+                key_file_name,
+                &error
+                )
+       ) {
+        fprintf(stderr, "Cannot write key file \"%s\": %s\n", key_file_name, error->message);
+    }
+
+    if (!flock(fd, LOCK_UN)) {
+        perror("save_index");
+        return;
+    }
+
+    if (!close(fd)) {
+        perror("save_index");
+    }
+}
+
--- xahead.h
+++ xahead.h
@@ -1,3 +1,6 @@
 #define CONF_NO_EXEC 0
 
+void configure_key_file_name(char *filename);
 int load_index(char *progname, int is_wine);
+void save_index(char *progname, int is_wine, int index);
+
